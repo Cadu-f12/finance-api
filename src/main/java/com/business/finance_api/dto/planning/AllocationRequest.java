c9f1802e3978253ea6ab67bd5f4e9cfe4439533f
Cadu-f12 <cadudailha68@gmail.com>
2026-09-05 16:15:42 -0300

    fix: Fix the none validations on AllocationRequest

--- src/main/java/com/business/finance_api/dto/planning/AllocationRequest.java
+++ src/main/java/com/business/finance_api/dto/planning/AllocationRequest.java
@@ -1,13 +1,13 @@
 package com.business.finance_api.dto.planning;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record AllocationRequest(
-        @NotEmpty(message = "Investment modality must not be blank.")
+        @NotBlank(message = "Investment modality must not be blank.")
         String modality,
         @Positive(message = "Investment percentage must be greater than or equal to 0.")
         @NotNull(message = "Investment percentage must not be blank.")
--- src/main/java/com/business/finance_api/dto/planning/InvestmentRequest.java
+++ src/main/java/com/business/finance_api/dto/planning/InvestmentRequest.java
@@ -1,12 +1,13 @@
 package com.business.finance_api.dto.planning;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record InvestmentRequest(
         @NotEmpty(message = "At least one investment allocation must be provided.")
-        List<AllocationRequest> allocations
+        List<@Valid AllocationRequest> allocations
 ) {
     @Override
     public String toString() {
